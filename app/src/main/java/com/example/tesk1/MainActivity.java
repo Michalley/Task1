1aa269381e1056a013f7a4ad7a186c1e650d326a
michalley <ml5877@bs.amalnet.k12.il>
2018-06-13 13:02:38 +0300

    1st commit

---
+++ app/src/main/java/com/example/tesk1/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.tesk1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.btn);
+    }
+
+    public void clicked(View view) {
+
+        btn.setText("Oh, yea, I've been clicked!");
+    }
+}
